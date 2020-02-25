58b91554834f837712444fec9562a212bc0fee7d
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-25 19:40:16 +0000

    added executable bin file

--- lib/miraculous_cli.rb
+++ lib/miraculous_cli.rb
@@ -1,7 +1,10 @@
-require "miraculous_cli/version"
-require 'pry'
-require 'nokogiri'
-require 'open-uri'
+require_relative "./miraculous_cli/version"
+#require 'pry'
+#require 'nokogiri'
+#require 'open-uri'
+
+require_relative './miraculous_cli/cli'
+
 
 module MiraculousCli
   class Error < StandardError; end
---
+++ lib/miraculous_cli/cli.rb
@@ -0,0 +1,9 @@
+#CLI controller
+class MiraculousCli::CLI
+  
+  def call
+    puts "Welcome to your Miraculous Ladybug Episode Sorter!"
+  end
+  
+  
+end
